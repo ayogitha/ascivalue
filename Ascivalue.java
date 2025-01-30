0f3749b73af6b3c3e1adf844d3febc415a448de5
ayogitha <eliteresourcenetwork@gmail.com>
2025-01-30 22:04:40 +0530

    Add files via upload

---
+++ Ascivalue.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ascivalue {
+    public static void main(String[] args) {
+        Scanner src=new Scanner(System.in);
+        System.out.println("enter a character ");
+        char ch=src.next().charAt(0);
+        System.out.println(" asci value are  "+(int)(ch));
+
+        
+    }
+    
+}
